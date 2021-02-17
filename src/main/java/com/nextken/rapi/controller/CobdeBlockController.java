c421f6f7c7d01dcf6da61ef19cc3a17b2a0dc51f
yilmazerd <yilmazerdem@gmail.com>
2021-02-16 22:59:04 -0500

    MAIN, some update

--- src/main/java/com/nextken/rapi/controller/CobdeBlockController.java
+++ src/main/java/com/nextken/rapi/controller/CobdeBlockController.java
@@ -34,33 +34,7 @@ public class CobdeBlockController {
         return "hello-world";
     }
 
-//    @CrossOrigin(origins = {"null", "https://instantfunction.com"})
-//    @PostMapping(path = "/formation/codeblock", consumes = "application/json", produces = "application/json")
-//    public ResponseEntity<CBResponse> postFormationController(@RequestBody CBRequest cbRequest) throws Exception{
-//        CBResponse cbResponse;
-//        String errorStatement = null;
-//        if (cbRequest == null) {
-//            errorStatement += "Invalid entry/n";
-//        } else {
-//            if (cbRequest.getCode() == null) {
-//                errorStatement += "Invalid input/n";
-//            }
-//        }
-//
-//        if (errorStatement!=null){
-//            CBResponseError cbResponseError = new CBResponseError(errorStatement);
-//            cbResponse = new CBResponse(cbResponseError);
-//            return ResponseEntity.ok()
-//                    .body(cbResponse);
-//        }
-//
-//        cbResponse = codeBlockService.create(cbRequest);
-//
-//        return ResponseEntity.ok()
-//                .body(cbResponse);
-//    }
-
-    @CrossOrigin(origins = {"null", "https://instantfunction.com"})
+    @CrossOrigin(origins = {"null", "http://localhost:63342", "https://instantfunction.com","localhost:63342"})
     @PostMapping(path = "/formation/codeblock2", produces = "application/json")
     public ResponseEntity<CBResponse> postFormationController2(
             @RequestBody String cbRequestIn,
@@ -88,8 +62,6 @@ public class CobdeBlockController {
                 compiler = CBCompiler.valueOf(headers.get("compiler"));
         } catch (Exception e) {
             throw new IllegalArgumentException("Incorrect compiler " + e.toString());
-            // DEFAULT TO JAVA
-            // compiler = CBCompiler.JAVA11;
         }
 
         CBRequest cbRequest = new CBRequest(cbRequestIn, "primaryKey","secondaryKey",compiler);
@@ -100,139 +72,4 @@ public class CobdeBlockController {
         return ResponseEntity.ok()
                 .body(cbResponse);
     }
-
-    // TODO: Erase everything below this line
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-    ObjectMapper mapper = new ObjectMapper();
-    RestTemplate restTemplate = new RestTemplate();
-    JSONObject jsonObject = new JSONObject();
-    JsonNode root = null;
-    HttpHeaders responseHeaders = new HttpHeaders();
-
-    @GetMapping("/greeting")
-    public ResponseEntity<Object> getController() {
-
-        //String resourceURL = "https://raw.githubusercontent.com/yilmazerd/simplerest01/nextken-000/src/main/resources/template1.json";
-        //String resourceURL = "https://tools.learningcontainer.com/sample-json.json";
-        String resourceURL = "http://www.sci.utah.edu/~macleod/docs/txt2html/sample.txt";
-        ResponseEntity<String> response  = restTemplate.getForEntity(resourceURL, String.class);
-// Set the response for JSON BODY
-        Object responseBody = getResponseBody(response, ResponseType.TEXT);
-
-// Set Response headers
-        responseHeaders.set(
-                "Example-Header-Sample", "This-is-my-sample"
-        );
-
-        return ResponseEntity.ok()
-                .headers(responseHeaders)
-                .body(responseBody);
-    }
-
-
-
-    @PostMapping("/greetPost")
-    @CrossOrigin(origins = "*")
-    public ResponseEntity<Object> postController() {
-
-        //String resourceURL = "https://raw.githubusercontent.com/yilmazerd/simplerest01/nextken-000/src/main/resources/template1.json";
-        //String resourceURL = "https://tools.learningcontainer.com/sample-json.json";
-        String resourceURL = "http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm";
-        ResponseEntity<String> response  = restTemplate.getForEntity(resourceURL, String.class);
-// Set the response for JSON BODY
-        Object responseBody = getResponseBody(response, ResponseType.TEXT);
-
-// Set Response headers
-        responseHeaders.set(
-                "Example-Header-Sample", "This-is-my-sample"
-        );
-
-        return ResponseEntity.ok()
-                .headers(responseHeaders)
-                .body(responseBody);
-    }
-
-Object getResponseBody(ResponseEntity<String> response, ResponseType responseType) {
-    Object object = new Object();
-    switch (responseType) {
-        case JSON:
-            try {
-                LOGGER.log(Level.INFO,"Converted object");
-                root = mapper.readTree(response.getBody());
-                object = root;
-                LOGGER.log(Level.INFO,"Got JSON");
-                LOGGER.log(Level.INFO,jsonObject.toString());
-            } catch (Exception e) {
-                LOGGER.log(Level.INFO,"Got Exception" + e.toString());
-            }
-        case TEXT:
-            try {
-                object = response.getBody();
-                LOGGER.log(Level.INFO,"Using Text");
-            } catch (Exception e) {
-                LOGGER.log(Level.INFO,"Error converting text" + e.toString());
-            }
-    }
-
-    return object;
-}
-
-enum ResponseType{
-    JSON,
-    TEXT
-}
-
-class Content {
-
-        private String contentUrl;
-        private ResponseType responseType;
-        private int httpStatus = 200;
-        private Map<String, String> headers;
-
-        Content(String contentUrl, ResponseType responseType) {
-            this.contentUrl = contentUrl;
-            this.responseType = responseType;
-        }
-        Content(String contentUrl, ResponseType responseType, int httpStatus){
-            this.contentUrl = contentUrl;
-            this.responseType = responseType;
-            this.httpStatus = httpStatus;
-        }
-
-    Content(String contentUrl, ResponseType responseType, int httpStatus, Map<String, String> stringStringMap){
-        this.contentUrl = contentUrl;
-        this.responseType = responseType;
-        this.httpStatus = httpStatus;
-        this.headers = stringStringMap;
-    }
-
-    public int getHttpStatus() {
-        return httpStatus;
-    }
-
-    public ResponseType getResponseType() {
-        return responseType;
-    }
-
-    public String getContentUrl() {
-        return contentUrl;
-    }
-
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-}
-
 }
