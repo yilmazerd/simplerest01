2590edd4d497f7277ae9629399426c5f4f603e77
yilmazerd <yilmazerdem@gmail.com>
2021-09-28 23:58:14 -0400

    SMP-013 Modify the forward controller to post on lambda and modify response

--- src/main/java/com/nextken/rapi/controller/ForwardController.java
+++ src/main/java/com/nextken/rapi/controller/ForwardController.java
@@ -1,5 +1,6 @@
 package com.nextken.rapi.controller;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,15 +14,17 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 @RestController
 public class ForwardController {
 
+    private static Logger logger = Logger.getLogger(ForwardController.class.getName());
 
     // All controllers return the same
-    @PostMapping(path = "/forward",
-            produces = "application/json")
+    @PostMapping(path = "/forward", produces = "application/json")
     public Object postController(
             @Nullable @RequestBody String codeBulk) throws Exception{
 
@@ -57,7 +60,8 @@ public class ForwardController {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        URI uri = new URI("http://localhost:8000/user");
+        //URI uri = new URI("http://localhost:8000/user");
+        URI uri = new URI("https://a4m4a452gc.execute-api.us-east-1.amazonaws.com/QA");
 
 
         //HttpEntity<JsonNode> requestEntity = new HttpEntity<JsonNode>(null, jsonNode);
@@ -66,10 +70,27 @@ public class ForwardController {
         ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
 
         try {
-            JsonNode jsonNodeResponse = objectMapper.readTree(result.getBody());
+            objectMapper = new ObjectMapper();
+            objectMapper.configure(JsonGenerator.Feature.QUOTE_FIELD_NAMES, false);
+            objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+            objectMapper.configure(JsonParser.Feature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER, false);
+
+            // edit response
+            String s2 = result.getBody().toString();
+            String s2_3 = s2.substring(1,s2.length()-1).replace("\\","");
+            JsonNode jsonNodeResponse = objectMapper.readTree(s2_3);
+            logger.log(Level.INFO, "json is succesfull");
+            String s3 = "{\"nick\": \"cowtowncoder\"}";
+            logger.log(Level.INFO, s3);
+            logger.log(Level.INFO, result.getBody().toString());
+            logger.log(Level.INFO, s2);
+            logger.log(Level.INFO, s2_3);
+            JsonNode newNode = objectMapper.readTree(s2);
+
             return jsonNodeResponse;
+            //return newNode;
         } catch (Exception e) {
-            //
+            logger.log(Level.INFO, "Can not cast to jSON");
         }
 
         return result;
